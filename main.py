daa11630021c83ccbaedc98bd58551a8f76c95d0
Camilo UNAD <cabeltrancal@unadvirtual.edu.co>
2026-05-02 17:33:45 -0500

    feat: se agrega los menus principales

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from views.menu import mostrar_menu
+from views.cliente_view import menu_cliente
+from views.servicios_view import menu_servicios
+
+from utils.input_utils import obtener_opcion
 
 def main():
     while True:
         mostrar_menu()
-        opcion = input("Seleccione una opción: ")
-        
+        opcion = obtener_opcion("Seleccione una opción: ", ["1", "2", "3"])
         if opcion == "1":
+            menu_cliente()
             pass
-        elif opcion == "4":
-            break
+        elif opcion == "2":
+            menu_servicios()
+            pass
+        elif opcion == "3":
+             print("Saliendo del programa...")
+             break
 
 if __name__ == "__main__":
     main()
